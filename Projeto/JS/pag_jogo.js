82155d7b851eba0a439cdc53a42f933137b679f3
josie <99617660+miguelserpamedeiros@users.noreply.github.com>
2024-05-09 10:46:05 +0100

    Update pag_jogo.js

--- Projeto/JS/pag_jogo.js
+++ Projeto/JS/pag_jogo.js
@@ -13,3 +13,50 @@ if (compareImg(class1,class2,id1,id2)) {
 } else {
     console.log('As imagens são diferentes.');
 }
+
+
+const BOTAO_INICIA_JOGO = "start";
+
+const BOTAO_CANCELAR_JOGO = 'cancel';
+
+const SPAN_TEMPO_JOGO ="spanTempoJogo";
+
+let jogo = {
+    inicio: null,
+    tentativas: null
+  };
+
+let temporizadorTempoJogo = 0;
+
+function defineEventListenersParaElementosHTML() {
+  
+    document.getElementById(BOTAO_INICIA_JOGO).
+    addEventListener("click", iniciaJogo);
+
+    document.getElementById(BOTAO_CANCELAR_JOGO).
+    addEventListener("click", cancelaJogo)
+  }
+
+
+function iniciaJogo() {
+    jogo.inicio = Math.floor(Date.now() / 1000);
+    temporizadorTempoJogo = setInterval(mostraTempoJogo);
+  
+  }
+
+function mostraTempoJogo(){
+    let tempo_at = Math.floor(Date.now() / 1000) - jogo.inicio;
+    document.getElementById(SPAN_TEMPO_JOGO).innerHTML = tempo_at;
+}
+function principal() {
+
+    defineEventListenersParaElementosHTML();
+  }
+
+function cancelaJogo() {
+    clearInterval(temporizadorTempoJogo);
+    document.getElementById(SPAN_TEMPO_JOGO).innerHTML = 0;
+  }
+
+
+window.addEventListener("load", principal);
